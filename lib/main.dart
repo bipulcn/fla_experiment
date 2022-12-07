a85011d528c818b16e4b6066afd82efc4929b005
bipulcn <bipulcn@gmail.com>
2022-12-07 16:09:12 +0600

    update of them and page loading

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:practice/base/bottom.dart';
+import 'package:practice/base/route.dart';
+import 'base/base.dart';
 
 void main() {
   runApp(const MyApp());
@@ -41,28 +44,31 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have push it for not a single time:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: SafeArea(
+          child: Stack(children: <Widget>[
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+            ),
+            BasePage(),
+          ]),
+        ),
+        drawer: Drawer(
+          child: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.bottomRight,
+                  end: Alignment.topLeft,
+                  colors: [Color(0xFF324065), Color(0xFF4b597c)]),
+            ),
+            child: Routers(),
+          ),
+        ),
+        bottomNavigationBar:
+            BottomBar() // This trailing comma makes auto-formatting nicer for build methods.
+        );
   }
 }
